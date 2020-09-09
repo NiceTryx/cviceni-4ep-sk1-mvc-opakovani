f91d5bd27214ea847f9d1c19c749d9ff4b1a6d67
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-09-09 11:35:08 +0200

    Hotová struktura aplikace (chybí router)

--- src/index.php
+++ src/index.php
@@ -4,17 +4,21 @@ require_once "db.php";
 
 $url = $_SERVER["REQUEST_URI"];
 
-$odkud = strpos($url, "index.php") + strlen("index.php");
-$zajimava_cast = substr($url, $odkud);
+$zacatek = strpos($url, "index.php") + strlen("index.php");
+$zajimava_cast = substr($url, $zacatek);
 $casti_url = explode("/", $zajimava_cast);
 
-$controller = $casti_url[1];
-$akce = $casti_url[2];
-
+$controller = null;
+$akce = null;
 $parametry = [];
 
-for($i = 3; $i < count($casti_url); $i++)
-    $parametry[] = $casti_url[$i];
+if(count($casti_url) > 1)
+    $controller = $casti_url[1];
+if(count($casti_url) > 2)
+    $akce = $casti_url[2];
+if(count($casti_url) > 3)
+    for($i = 3; $i < count($casti_url); $i++)
+        $parametry[] = $casti_url[$i];
 
 // z URL jsme ziskali pozadovany controller, akci a jeji pripadne parametry
 
---
+++ src/views/layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC PHP</title>
+</head>
+<body>
+    <header>
+        <h1>MVC PHP</h1>
+    </header>
+    <main>
+        <?php require_once "router.php"; ?>
+    </main>
+    <footer>
+        <p>&copy; 2020 Jakub Šenkýř</p>
+    </footer>
+</body>
+</html>
